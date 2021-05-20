c2f4342b8727e81cde800922957c5c73f6ae0b45
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2021-05-20 14:53:06 +0200

    Init project, add basic client code

---
+++ client/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessagesForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName;
